06945b80ee3f83c3f113df6665a3e64df5d50482
subasrirangasamy <subasrirangasamy@gmail.com>
2026-01-09 13:05:44 +0530

    Add Resume class with content handling

---
+++ resume.java
@@ -0,0 +1,12 @@
+public class Resume {
+
+    private String content;
+
+    public Resume(String content) {
+        this.content = content.toLowerCase();
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
